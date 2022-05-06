d62f6b402febe6e715b56e41f893a6c24065a60d
Emmanuel Okeke <emmanuelokeke506@gmail.com>
2022-05-06 03:04:56 -0500

    first commit

---
+++ lib/notifier/food_notifier.dart
@@ -0,0 +1,17 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:foody/model/food.dart';
+
+class FoodNotifier with ChangeNotifier {
+  List<Food> _foodList = [];
+
+  UnmodifiableListView<Food> get foodList {
+    return UnmodifiableListView(_foodList);
+  }
+
+  set foodList(List<Food> foodList) {
+    _foodList = foodList;
+    notifyListeners();
+  }
+}
---
+++ lib/screens/navigation_bar.dart
@@ -0,0 +1,56 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:foody/screens/add_post.dart';
+import 'package:foody/screens/home_page.dart';
+import 'package:foody/screens/profile_page.dart';
+
+class NavigationBarPage extends StatefulWidget {
+  int selectedIndex;
+  NavigationBarPage({@required this.selectedIndex});
+  @override
+  _NavigationBarPageState createState() => _NavigationBarPageState();
+}
+
+class _NavigationBarPageState extends State<NavigationBarPage> {
+  final List<Widget> _children = [
+    HomePage(),
+    ImageCapture(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: _children[widget.selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.white,
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: Colors.white,
+        height: 50,
+        index: widget.selectedIndex,
+        onTap: (index) {
+          setState(() {
+            widget.selectedIndex = index;
+          });
+        },
+        items: <Widget>[
+          Icon(
+            Icons.home,
+            size: 26,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.add_box,
+            size: 26,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.account_circle,
+            size: 26,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
